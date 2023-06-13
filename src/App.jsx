830ad4ce51ef0cfab7ee68366d5ef0247ce74ec2
Gabriel-Leal <gabriel.leal.s.oliveira@gmail.com>
2023-06-13 19:46:41 -0400

    added data + iteration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,65 @@
+/* eslint-disable react/jsx-key */
 import { Post } from "./components/Post";
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
 
 import "./global.css";
 import styles from "./App.module.css";
+
+// author: { avatar_url: "", name: "", role: "" }
+// publishedAt: Date
+// content: string
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/Gabriel-Leal.png",
+      name: "Gabriel Sala",
+      role: "Frontend Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Lorem ipsum dolor" },
+      { type: "paragraph", content: "sit amet consectetur adipisicing elit." },
+      {
+        type: "paragraph",
+        content:
+          "Tempore culpa, dignissimos sint accusantium natus tenetur quasi quas, dolore",
+      },
+      {
+        type: "paragraph",
+        content:
+          "mollitia dicta rem cupiditate accusamus reiciendis minus pariatur ullam obcaecati eveniet id.",
+      },
+      { type: "link", content: "gabriel.sala/Frontend" },
+    ],
+    publishedAt: new Date("2023-01-01 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/tatiane-leal.png",
+      name: "Tatiane Sala",
+      role: "Frontend Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Lorem ipsum dolor" },
+      { type: "paragraph", content: "sit amet consectetur adipisicing elit." },
+      {
+        type: "paragraph",
+        content:
+          "Tempore culpa, dignissimos sint accusantium natus tenetur quasi quas, dolore",
+      },
+      {
+        type: "paragraph",
+        content:
+          "mollitia dicta rem cupiditate accusamus reiciendis minus pariatur ullam obcaecati eveniet id.",
+      },
+      { type: "link", content: "tatiane.sala/Frontend" },
+    ],
+    publishedAt: new Date("2023-01-10 20:00:00"),
+  },
+];
 
 export function App() {
   return (
@@ -12,14 +68,22 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Gabriel Sala"
-            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore culpa, dignissimos sint accusantium natus tenetur quasi quas, dolore, mollitia dicta rem cupiditate accusamus reiciendis minus pariatur ullam obcaecati eveniet id."
-          />
-          <Post
-            author="Tatiane Sala"
-            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore culpa, dignissimos sint accusantium natus tenetur quasi quas, dolore, mollitia dicta rem cupiditate accusamus reiciendis minus pariatur ullam obcaecati eveniet id."
-          />
+          {posts.map((item) => (
+            <Post
+              author={item.author}
+              content={item.content}
+              publishedAt={item.publishedAt}
+            />
+          ))}
+          {/* {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })} */}
         </main>
       </div>
     </>
