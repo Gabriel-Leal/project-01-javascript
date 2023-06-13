272e3e67a76f45f1f5699f640a5ec2e4efd98dcf
Gabriel-Leal <gabriel.leal.s.oliveira@gmail.com>
2023-06-12 23:41:06 -0400

    add sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import { Post } from "./Post";
+import { Post } from "./components/Post";
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
+import "./global.css";
+import styles from "./App.module.css";
+
 export function App() {
   return (
     <>
-      <h1>Hello world</h1>
-      <Post
-        author="Gabriel Sala"
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore culpa, dignissimos sint accusantium natus tenetur quasi quas, dolore, mollitia dicta rem cupiditate accusamus reiciendis minus pariatur ullam obcaecati eveniet id."
-      />
-      <Post
-        author="Tatiane Sala"
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore culpa, dignissimos sint accusantium natus tenetur quasi quas, dolore, mollitia dicta rem cupiditate accusamus reiciendis minus pariatur ullam obcaecati eveniet id."
-      />
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Gabriel Sala"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore culpa, dignissimos sint accusantium natus tenetur quasi quas, dolore, mollitia dicta rem cupiditate accusamus reiciendis minus pariatur ullam obcaecati eveniet id."
+          />
+          <Post
+            author="Tatiane Sala"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore culpa, dignissimos sint accusantium natus tenetur quasi quas, dolore, mollitia dicta rem cupiditate accusamus reiciendis minus pariatur ullam obcaecati eveniet id."
+          />
+        </main>
+      </div>
     </>
   );
 }
---
+++ src/components/Post.jsx
@@ -0,0 +1,40 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <>
+      <article className={styles.post}>
+        <header>
+          <div className={styles.author}>
+            <img
+              className={styles.avatar}
+              src="https://github.com/Gabriel-Leal.png"
+            />
+            <div className={styles.authorInfo}>
+              <strong>Gabriel Sala</strong>
+              <span>Frontend Developer</span>
+            </div>
+          </div>
+
+          <time title="2023-01-01 04:13:30" dateTime="2023-01-01 04:13:30">
+            Published 1hour ago
+          </time>
+        </header>
+        <div className={styles.content}>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor
+            nulla officia itaque mollitia ab quos quae autem doloribus? Id
+            tenetur unde atque et error facere, natus quasi soluta repudiandae
+            officiis!
+          </p>
+          <p>
+            <a href="#">gabriel.design/doctorcare</a>
+          </p>
+          <p>
+            <a href="#">#react #newproject</a>
+          </p>
+        </div>
+      </article>
+    </>
+  );
+}
